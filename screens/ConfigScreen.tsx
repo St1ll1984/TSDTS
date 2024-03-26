13205339f6b8b9b1e4865b3349c6946c8ad6eb20
Ruslan Shyrogorov <ruslanshirogorov@gmail.com>
2024-03-26 22:01:40 +0200

    add input of company

--- screens/ConfigScreen.tsx
+++ screens/ConfigScreen.tsx
@@ -1,34 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { HomeStackNavigatorProp } from '../types/type';
+import { HomeStackNavigatorProp, IUser } from '../types/type';
 import { TextInput } from 'react-native-gesture-handler';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export interface IUser {
-	id: string;
-	extraID: string;
-	name: string;
-}
+import usersDb from '../temporaryDB/users';
 
 const ConfigScreen = () => {
 	const [company, setCompany] = useState<string>('');
-
 	const [users, setUsers] = useState<IUser[]>([]);
-	console.log(company);
+	const [currentUser, setCurrentUser] = useState<IUser>();
+	const onPressCompanyBtn = () => {
+		setUsers(usersDb);
+	};
 
 	return (
-		<View>
+		<View style={styles.container}>
 			<View>
-				<TextInput
-					placeholder="ЄДРПОУ"
-					value={company}
-					keyboardType="numeric"
-					onChangeText={(value) => setCompany(value.toString())}
-				/>
+				<View>
+					<Text>ЄДРПОУ</Text>
+					<TextInput
+						style={
+							company ? [styles.input, { borderColor: 'gray' }] : styles.input
+						}
+						placeholder="ЄДРПОУ"
+						value={company}
+						keyboardType="numeric"
+						onChangeText={(value) => setCompany(value)}
+					/>
+					<TouchableOpacity
+						style={
+							!company
+								? [styles.button, { backgroundColor: 'gray' }]
+								: styles.button
+						}
+						disabled={!company}
+						onPress={onPressCompanyBtn}
+					>
+						<Text style={styles.buttonText}>Дальше</Text>
+					</TouchableOpacity>
+				</View>
 			</View>
-			{/*{users.length && <View></View>}*/}
+			{users.length > 0 ? (
+				<View>
+					<Text>FlatList</Text>
+				</View>
+			) : null}
 		</View>
 	);
 };
@@ -36,15 +54,41 @@ const ConfigScreen = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'stretch',
-		justifyContent: 'center',
-		textAlignVertical: 'auto',
+		backgroundColor: '#eee',
+		padding: 15,
+	},
+	input: {
+		backgroundColor: 'white',
+		borderColor: 'red',
+		width: '100%',
+		borderWidth: 1,
+		borderRadius: 8,
+		padding: 10,
+		height: 45,
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		marginTop: 5,
+		marginBottom: 10,
+		fontSize: 20,
+	},
+	inputFocus: {
+		borderColor: 'green',
 	},
 	button: {
-		paddingTop: 100,
-		paddingBottom: 100,
-		borderRadius: 60,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingVertical: 12,
+		paddingHorizontal: 32,
+		borderRadius: 20,
+		elevation: 3,
+		backgroundColor: 'blue',
+	},
+	buttonText: {
+		fontSize: 16,
+		fontWeight: 'bold',
+		letterSpacing: 1.2,
+		textTransform: 'uppercase',
+		color: 'white',
 	},
 });
 
